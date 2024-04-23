5b4f4df569509d090a104403823ae922b873fc47
ibnatysalsabila <salsajpr01@gmail.com>
2024-04-23 17:48:08 +0700

    memasukan void input

--- ooppdua/ooppdua.cpp
+++ ooppdua/ooppdua.cpp
@@ -7,3 +7,10 @@ private:
 	float panjang, Lebar;
 public:
 	float Luas;
+
+    void input() { // methode input persegi panjang 
+        cout << "Masukan Panjangnya = ";
+        cin >> panjang;
+        cout << "Masukan Lebarnya = ";
+        cin >> Lebar;
+    }
